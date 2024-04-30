208e69e09d7799926c31a0bede2a3801595dd612
Olasunkanmi Oyinlola <olasunkanmioyinlola@Olasunkanmis-Mac-mini.local>
2024-04-30 20:52:57 +0800

    refactor the chatManager module

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,17 @@
 import * as markdownit from "markdown-it";
+import * as vscode from "vscode";
+
+type GetConfigValueType<T> = (key: string) => T | undefined;
 
 export const formatText = (text: string): string => {
   const md = markdownit();
   return md.render(text);
+};
+
+export const getConfigValue: GetConfigValueType<any> = <T>(key: string): T | undefined => {
+  return vscode.workspace.getConfiguration().get<T>(key);
+};
+
+export const vscodeErrorMessage = (error: string, metaData?: any) => {
+  return vscode.window.showErrorMessage(error);
 };
