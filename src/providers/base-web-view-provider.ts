754ca786ce8b4edcf92a803619cefcf184c6c083
AKZ <75739105+darkelfs56@users.noreply.github.com>
2024-12-11 19:18:55 +0800

    Changes are: - Modified localResourceRoots to include related files for using folder "webview-ui" - Commented out knowledgeBaseDocs code because not sure how to do it. - Add setInterval every 3 seconds that will call getChatCss and postMessage to react webview-ui if any changes to update the current stylings.

--- src/providers/base-web-view-provider.ts
+++ src/providers/base-web-view-provider.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import { getWebviewContent } from "../webview/chat";
 import { formatText } from "../utils";
 import { FileUploader } from "../events/file-uploader";
+import { getChatCss } from "../webview/chat_css";
 
 let _view: vscode.WebviewView | undefined;
 export abstract class BaseWebViewProvider {
@@ -14,7 +15,7 @@ export abstract class BaseWebViewProvider {
     private readonly _extensionUri: vscode.Uri,
     protected readonly apiKey: string,
     protected readonly generativeAiModel: string,
-    context: vscode.ExtensionContext,
+    context: vscode.ExtensionContext
   ) {
     this._context = context;
   }
@@ -26,13 +27,17 @@ export abstract class BaseWebViewProvider {
 
     const webviewOptions: vscode.WebviewOptions = {
       enableScripts: true,
-      localResourceRoots: [this._extensionUri],
+      localResourceRoots: [
+        this._extensionUri,
+        vscode.Uri.joinPath(this._extensionUri, "out"),
+        vscode.Uri.joinPath(this._extensionUri, "webview-ui/build"),
+      ],
     };
     webviewView.webview.options = webviewOptions;
 
     if (!this.apiKey) {
       vscode.window.showErrorMessage(
-        "API key not configured. Check your settings.",
+        "API key not configured. Check your settings."
       );
       return;
     }
@@ -40,20 +45,42 @@ export abstract class BaseWebViewProvider {
     this.setupMessageHandler(
       this.apiKey,
       this.generativeAiModel,
-      this.currentWebView,
+      this.currentWebView
     );
   }
 
   private async setWebviewHtml(view: vscode.WebviewView): Promise<void> {
-    const codepatterns: FileUploader = new FileUploader(this._context);
-    const knowledgeBaseDocs: string[] = await codepatterns.getFiles();
-    view.webview.html = getWebviewContent(knowledgeBaseDocs);
+    // const codepatterns: FileUploader = new FileUploader(this._context);
+    // const knowledgeBaseDocs: string[] = await codepatterns.getFiles();
+
+    if (this.currentWebView?.webview == undefined) {
+      throw Error(
+        "Error on no webview for currentWebView for base-web-view-provider"
+      );
+    }
+
+    view.webview.html = getWebviewContent(
+      this.currentWebView?.webview,
+      this._extensionUri
+    );
+
+    setInterval(async () => {
+      let chatCss = await getChatCss();
+      if (chatCss == false) {
+        return;
+      }
+
+      this.currentWebView?.webview.postMessage({
+        type: "updateStyles",
+        payload: { chatCss: chatCss },
+      });
+    }, 3000);
   }
 
   private setupMessageHandler(
     apiKey: string,
     modelName: string,
-    _view: vscode.WebviewView,
+    _view: vscode.WebviewView
   ): void {
     try {
       _view.webview.onDidReceiveMessage(async (message) => {
@@ -61,7 +88,7 @@ export abstract class BaseWebViewProvider {
           const response = await this.generateResponse(
             apiKey,
             modelName,
-            formatText(message.message),
+            formatText(message.message)
           );
           if (response) {
             this.sendResponse(formatText(response), "bot");
@@ -76,11 +103,11 @@ export abstract class BaseWebViewProvider {
   abstract generateResponse(
     apiKey?: string,
     name?: string,
-    message?: string,
+    message?: string
   ): Promise<string | undefined>;
 
   abstract sendResponse(
     response: string,
-    currentChat?: string,
+    currentChat?: string
   ): Promise<boolean | undefined>;
 }
