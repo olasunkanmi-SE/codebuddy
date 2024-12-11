6d851ce38bc6f87f0e31101b9530dc5100000d16
AKZ <75739105+darkelfs56@users.noreply.github.com>
2024-12-11 19:13:02 +0800

    Modified chat.ts file to create a new getWebviewContent that expects Webview and Uri types from "vscode"

--- src/webview/chat.ts
+++ src/webview/chat.ts
@@ -1,5 +1,59 @@
-import { chartComponent } from "./chat_html";
+import { Webview, Uri } from "vscode";
+import { getUri, getNonce } from "../utils";
+// import { chartComponent } from "./chat_html";
 
-export function getWebviewContent(docs: string[]): string {
-  return chartComponent(docs);
+// export function getWebviewContent(docs: string[]): string {
+//   return chartComponent(docs);
+// }
+
+export function getWebviewContent(webview: Webview, extensionUri: Uri) {
+  // The CSS file from the React build output
+  const stylesUri = getUri(webview, extensionUri, [
+    "webview-ui",
+    "build",
+    "assets",
+    "index.css",
+  ]);
+  // The JS file from the React build output
+  const scriptUri = getUri(webview, extensionUri, [
+    "webview-ui",
+    "build",
+    "assets",
+    "index.js",
+  ]);
+
+  const nonce = getNonce();
+
+  // <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'unsafe-inline'; script-src 'nonce-${nonce}';">
+
+  // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
+  return /*html*/ `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="${stylesUri}">
+        <title>Ai</title>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script type="module" nonce="${nonce}" src="${scriptUri}"></script>
+      </body>
+    </html>
+  `;
 }
